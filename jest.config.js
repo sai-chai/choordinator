7a9b95d69a029d61cbe75cdb297903c0c8a1d5c1
Sai Chimata <saichimata@gmail.com>
2019-11-11 23:56:04 -0600

    eslint --fix + tonal packages

--- jest.config.js
+++ jest.config.js
@@ -28,7 +28,7 @@ module.exports = {
 
    // An array of regexp pattern strings used to skip coverage collection
    coveragePathIgnorePatterns: [
-      "/node_modules/"
+      "/node_modules/",
    ],
 
    // A list of reporter names that Jest uses when writing coverage reports
@@ -66,7 +66,7 @@ module.exports = {
    // An array of directory names to be searched recursively up from the requiring module's location
    moduleDirectories: [
       "node_modules",
-      "src"
+      "src",
    ],
 
    // An array of file extensions your modules use
@@ -144,13 +144,13 @@ module.exports = {
    // The glob patterns Jest uses to detect test files
    testMatch: [
       "**/__tests__/**/*.[jt]s?(x)",
-      "**/?(*.)+(spec|test).[tj]s?(x)"
+      "**/?(*.)+(spec|test).[tj]s?(x)",
    ],
 
    // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
    testPathIgnorePatterns: [
-      "/node_modules/"
-   ]
+      "/node_modules/",
+   ],
 
    // The regexp pattern or array of patterns that Jest uses to detect test files
    // testRegex: [],
